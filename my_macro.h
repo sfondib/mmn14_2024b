bc6a01d9448f2e14e3dc7d8f90981cf017ce56ed
sfondib <stavfondi@gmail.com>
2024-07-10 22:52:39 +0300

    Update my_macro.h
    add MOVE_TO_NOT_WHITE

--- my_macro.h
+++ my_macro.h
@@ -7,3 +7,11 @@
 /* Boolean (t/f) helpers */
 #define FALSE = 0
 #define TRUE = 1
+
+/**
+* For both runs
+* Moves the index to the next place in string where there is no white char 
+*/
+#define MOVE_TO_NOT_WHITE(string, index) \
+        for (;string[(index)] && (string[(index)] == '\t' || string[(index)] == ' '); (++(index)))\
+        ;
