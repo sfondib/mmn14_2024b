676aa0e24fe80fcb233ba4d8dab25c465ebf1210
sfondib <stavfondi@gmail.com>
2024-07-10 20:05:53 +0300

    Create my_macro.h
    קיצורים- מאקרואיים

---
+++ my_macro.h
@@ -0,0 +1,9 @@
+/* Maximum length of a single source line  */
+#define MAX_LINE_LENGTH 80
+
+/* Initial IC value */
+#define IC_INIT_VALUE 100
+
+/* Boolean (t/f) helpers */
+#define FALSE = 0
+#define TRUE = 1
