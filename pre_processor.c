8ee412f108628827927e7d69889a34f3ad60c945
Ofir Tzriker <ofirc36@gmail.com>
2024-07-10 15:48:26 +0300

    Added README.md and pre_processor.c

---
+++ pre_processor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+/*
+The function receives .as files with assembly instructions and
+replaces all macro calls with the matching definitions
+
+@param1
+@param2
+@param3
+
+@return
+*/
+int main(void) {
+	return 0;
+}
