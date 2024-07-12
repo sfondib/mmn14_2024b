56ea19e7a7e43372ca64b802e57e77debd0bb4d2
sfondib <stavfondi@gmail.com>
2024-07-12 13:05:16 +0300

    Update my_macro.h

--- my_macro.h
+++ my_macro.h
@@ -1,6 +1,9 @@
 /* Maximum length of a single source line  */
 #define MAX_LINE_LEN 80
 
+/* Maximum size of code image */
+#define CODE_IMG_MAX_LENGTH 1200
+
 /* Macro rules */
 #define NUM_INVALID_MACRO_NAMES	20
 #define MACRO_START			        "macr"
@@ -19,5 +22,12 @@ typedef enum booleans {
 * Moves the index to the next place in string where there is no white char 
 */
 #define MOVE_TO_NOT_WHITE(string, index) \
-        for (;string[(index)] && (string[(index)] == '\t' || string[(index)] == ' '); (++(index)))\
+        for (;string[(index)] && (string[(index)] == '\t' || string[(index)] == ' '); (++(index))) \
         ;
+
+/* for accembler file */
+typedef struct line_info {
+	long line_number;
+	char *file_name;
+	char *content; /* Line content (source) */
+} line_info;
