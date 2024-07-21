c833ad3c6a3af655f152b3cd413187b2de0f20df
sfondib <stavfondi@gmail.com>
2024-07-21 23:20:55 +0300

    Create helpful.h

---
+++ helpful.h
@@ -0,0 +1,8 @@
+/* in helpul file we will find helpful functions that will take part in the whole program */
+
+/**
+ * Allocates memory in required size, if process has been failed - exits the program
+ * @param size - the size to allocate in bytes
+ * @return a generic pointer to allocated memory
+ */
+void *malloc_check(long size);
