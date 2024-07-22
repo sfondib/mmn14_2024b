db488ca698af3f82d39fa34be8d7a90c7b55f8c7
sfondib <stavfondi@gmail.com>
2024-07-22 20:20:59 +0300

    Update table.h

--- table.h
+++ table.h
@@ -1,20 +1,44 @@
+#ifndef TABLE_H
+#define TABLE_H
 /* Libraries*/
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/* Pointer to table's entry */
+typedef struct item* table;
+
+/* Symbol type of item in the table */
+typedef enum symbol_type {
+    CODE_SYMBOL,
+    DATA_SYMBOL,
+    EXTERNAL_SYMBOL,
+    EXTERNAL_REFERENCE,
+    ENTRY_SYMBOL
+} symbol_type;
+
+/* Table item */
+typedef struct item {
+    table next; /* Next item */
+    long value; /* Item's Address */
+    char *key; /* item name (a string) */
+    symbol_type type; /* Symbol type */
+} table_item;
 
 /**
- * Add a new item to the table, in a sort way
+ * Add a new item to the table, in a sorted way
  * @param tbl - table's pointer
  * @param value - the value of the new item
- * @param enter - the entry for the new value
- * @param type - the he type of the entry to insert
+ * @param item - the entry for the new value
+ * @param type - the type of the entry to insert
  */
-void add_item_to_table(table *tbl, long value, char *enter, symbol_type type);
+void add_item_to_table(table *tbl, long value, char *item, symbol_type type);
 
 /**
  * Release all memory related to the table
- * @param tbl - the table to get free
+ * @param tbl - the table to be freed
  */
 void free_table(table tbl);
+
+#endif /* TABLE_H */
