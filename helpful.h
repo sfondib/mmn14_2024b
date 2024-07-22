428a2a8fd6870f92f1e424340adbee44772a208a
sfondib <stavfondi@gmail.com>
2024-07-22 20:22:24 +0300

    Update helpful.h

--- helpful.h
+++ helpful.h
@@ -1,4 +1,10 @@
 /* in helpul file we will find helpful functions that will take part in the whole program */
+#ifndef HELPFUL_H
+#define HELPFUL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * Allocates memory in required size, if process has been failed - exits the program
@@ -6,3 +12,5 @@
  * @return a generic pointer to allocated memory
  */
 void *malloc_check(long size);
+
+#endif /* HELPFUL_H */
