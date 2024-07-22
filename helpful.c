c41251e64c86581d84455215deb5ef0e44f33bb4
sfondib <stavfondi@gmail.com>
2024-07-22 20:22:46 +0300

    Update helpful.c

--- helpful.c
+++ helpful.c
@@ -1,10 +1,5 @@
 #include "helpful.h"
 
-/**
- * Allocates memory in required size, if process has been failed - exits the program
- * @param size - the size to allocate in bytes
- * @return a generic pointer to allocated memory
- */
 void *malloc_check(long size) {
     void *ptr = malloc(size);
     if (ptr == NULL) {
