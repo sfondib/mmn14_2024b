b4dd5b1314b77b0eacae9c5b4931429b3b926746
sfondib <stavfondi@gmail.com>
2024-08-03 13:17:25 +0300

    Update helpful.c

--- helpful.c
+++ helpful.c
@@ -15,14 +15,21 @@ void *malloc_check(long size) {
 int printErrorAccordingToLine(line_info line, char *message, ...) {
     int chars_printed;
     va_list args; /* for formatting */
- 
+    
     /* Print file name and line number */
     fprintf(stderr,"Error In %s:%ld: ", line.file_name, line.line_number);
     /* Initialize args to store all values after 'message' */
     va_start(args, message);
     chars_printed = vfprintf(stderr, message, args);
     va_end(args);
-    fprintf(stderr, "\n"); /* Print a new line after the err message - we shouldn't calculate '\n' */
-
+    fprintf(stderr, "\n"); /* Print a new line after error - we shouldn't calculate '\n' char */
     return chars_printed;
 }
+
+/* Instruction type of data, extern, entry, string */
+typedef enum instruction {
+    DATA_INST, /* data instruction */
+    STRING_INST, /* string instruction */
+    ENTRY_INST, /* entry instruction */
+    EXTERN_INST /* extern instruction */
+} instruction;
