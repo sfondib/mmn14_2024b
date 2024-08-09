e72b622c46d276413a2a935b89b7b561f34d4f6e
sfondib <stavfondi@gmail.com>
2024-08-09 16:22:37 +0300

    Update helpful.c

--- helpful.c
+++ helpful.c
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include "helpful.h"
 #include "my_macro.h"
+#include "table.h"
 
 void *malloc_check(long size) {
     void *ptr = malloc(size);
@@ -24,4 +25,19 @@ int printErrorAccordingToLine(line_info line, char *message, ...) {
     va_end(args);
     fprintf(stderr, "\n"); /* Print a new line after error - we shouldn't calculate '\n' char */
     return chars_printed;
+}
+
+int is_type_matching(table tbl, char *key, va_list arglist, int symbol_count) {
+    int i;
+    symbol_type current_type;
+
+    /* Loop through all the valid symbol types passed in the variadic arguments */
+    for (i = 0; i < symbol_count; i++) {
+        current_type = va_arg(arglist, symbol_type);
+        /* If the type matches and the key is the same, return 1 (match found) */
+        if (current_type == tbl->type && strcmp(key, tbl->key) == 0) {
+            return 1;
+        }
+    }
+    return 0; /* No match found */
 }
