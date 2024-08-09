96d609f730c4aad19f8f2720bfdcc4e9429ac781
sfondib <stavfondi@gmail.com>
2024-08-09 16:16:30 +0300

    Update table.c

--- table.c
+++ table.c
@@ -1,12 +1,11 @@
-/* Libraries*/
 #include "helpful.h"
-#include "table.h"									 
+#include "table.h"
 
 void add_item_to_table(table *tbl, long value, char *item, symbol_type type) {
     char *temp_item;
     table prev_item, curr_item, new_item;
     
-    /* allocate memory for new and 'temp' items */
+    /* Allocate memory for new and 'temp' items */
     new_item = (table) malloc_check(sizeof(table_item));
     temp_item = (char *) malloc_check(strlen(item) + 1);
     strcpy(temp_item, item);
@@ -16,7 +15,7 @@ void add_item_to_table(table *tbl, long value, char *item, symbol_type type) {
     new_item->value = value;
     new_item->type = type;
   
-    /* if table is empty, add the new item to the head of the table, or if new item's value is smallest then the current head */
+    /* If table is empty, add the new item to the head of the table, or if new item's value is smallest then the current head */
     if ((*tbl) == NULL || (*tbl)->value > value) {
         new_item->next = (*tbl);
         (*tbl) = new_item;
@@ -35,6 +34,16 @@ void add_item_to_table(table *tbl, long value, char *item, symbol_type type) {
     prev_item->next = new_item;
 }
 
+void free_table_memory(table tbl) {
+    table prev_item, curr_item = tbl;
+    while (curr_item != NULL) {
+        prev_item = curr_item;
+        curr_item = curr_item->next;
+        free(prev_item->key);
+        free(prev_item);
+    }
+}
+
 void add_value_if_type_match(table tbl, long to_add, symbol_type type) {
     /* Go over each item in the table and add the value to_add if the type matches */
     while (tbl != NULL) {
@@ -45,12 +54,25 @@ void add_value_if_type_match(table tbl, long to_add, symbol_type type) {
     }
 }
 
-void free_table_memory(table tbl) {
-    table prev_item, curr_item = tbl;
-    while (curr_item != NULL) {
-        prev_item = curr_item;
-        curr_item = curr_item->next;
-        free(prev_item->key);
-        free(prev_item);
+table_item *findType(table tbl, char *key, int symbol_count, ...) {
+    va_list arglist;
+
+    if (tbl == NULL) {
+        return NULL;
     }
+
+    va_start(arglist, symbol_count);
+
+    /* Iterate over the table entries */
+    do {
+        if (is_type_matching(tbl, key, arglist, symbol_count)) {
+            va_end(arglist);
+            return tbl;  /* Return the entry if a match is found */
+        }
+        va_end(arglist);  /* End the current iteration */
+        va_start(arglist, symbol_count);  /* Restart the arglist for the next iteration */
+    } while ((tbl = tbl->next) != NULL);
+
+    va_end(arglist);
+    return NULL;  /* No match found */
 }
