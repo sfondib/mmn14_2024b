19891524e16f401a5d63e57f04a2e55faa2046b5
sfondib <stavfondi@gmail.com>
2024-08-18 15:17:14 +0300

    Update second_run.h

--- second_run.h
+++ second_run.h
@@ -22,9 +22,11 @@ bool lineIndexPass(line_info line, long *ic, table *symbol_table);
  * @param file - Pointer to the source file to process.
  * @param file_name - The name of the source file.
  * @param symbol_table - Pointer to the symbol table.
+ * @param memory - Pointer to the instruction memory.
+ * @param dc - The data counter value.
  * @return TRUE if processing is successful, FALSE if errors occurred.
  */
-bool processFileSecondPass(FILE *file, const char *file_name, table *symbol_table);
+bool processFileSecondPass(FILE *file, const char *file_name, table *symbol_table, instruction *memory, long dc);
 
 /**
  * Encodes the operands of an instruction during the second pass.
@@ -36,5 +38,25 @@ bool processFileSecondPass(FILE *file, const char *file_name, table *symbol_tabl
  * @return TRUE if encoding is successful, FALSE if an error occurs.
  */
 bool encodeOperands(line_info line, long *ic, table *symbol_table, int *operand1_method, int *operand2_method);
+
+/**
+ * Creates the .ob file by writing the encoded instructions and data to it.
+ * @param file_name - The base name of the source file.
+ * @param memory - The memory structure holding the encoded instructions.
+ * @param ic - The final instruction counter value.
+ * @param dc - The final data counter value.
+ * @return TRUE if the file is created successfully, FALSE if an error occurs.
+ */
+bool createObjectFile(const char *file_name, instruction *memory, long ic, long dc);
+
+/**
+ * Creates the .ob file by writing the encoded instructions and data to it.
+ * @param file_name - The base name of the source file.
+ * @param memory - The memory structure holding the encoded instructions.
+ * @param ic - The final instruction counter value.
+ * @param dc - The final data counter value.
+ * @return TRUE if the file is created successfully, FALSE if an error occurs.
+ */
+bool createObjectFile(const char *file_name, instruction *memory, long ic, long dc);
 
 #endif /* SECOND_RUN_H */
